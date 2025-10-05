96b05a55e6b7dbc0458e034924a68773654bd595
LakhaniSinan <sinan.lakhani09@gmail.com>
2025-10-06 02:03:47 +0500

    asdsad

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
 import mongoose from "mongoose";
-const PORT = 8002;
-const MONGO_URI = "mongodb://localhost:27017/loan-app";
+const PORT = 3000;
+const MONGO_URI = "mongodb+srv://test:OGo2M1GfEMRjIvJu@cluster0.p2khqnt.mongodb.net/loan-app?retryWrites=true&w=majority&appName=Cluster0"
 
-mongoose.connect("mongodb+srv://test:OGo2M1GfEMRjIvJu@cluster0.p2khqnt.mongodb.net/loan-app?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(MONGO_URI)
   .then(() => {
     console.log(`MongoDB connected live ${MONGO_URI}`);
     app.listen(PORT, '0.0.0.0', () => {

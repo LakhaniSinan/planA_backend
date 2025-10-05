57e74cc80198f116271587af2a454ba751be9ac7
LakhaniSinan <sinan.lakhani09@gmail.com>
2025-10-06 01:42:52 +0500

    replaced main string

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const PORT = 8002;
 const MONGO_URI = "mongodb://localhost:27017/loan-app";
 
-mongoose.connect("mongodb://localhost:27017/loan-app")
+mongoose.connect("mongodb+srv://test:OGo2M1GfEMRjIvJu@cluster0.p2khqnt.mongodb.net/loan-app?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log(`MongoDB connected live ${MONGO_URI}`);
     app.listen(PORT, () => {

f139f44f6a907dc4e6fb67b4b09e4ce89b692b9b
LakhaniSinan <sinan.lakhani09@gmail.com>
2025-10-29 19:59:38 +0500

    ccccc

--- utilities/firebaseConfig.js
+++ utilities/firebaseConfig.js
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
 import admin from "firebase-admin";
+console.log(process.env.FIREBASE_CONFIG,"process.env.FIREBASE_CONFIGprocess.env.FIREBASE_CONFIG");
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
--- utilities/notification.js
+++ utilities/notification.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import admin from "./firebaseConfig.js";
 import { google } from "googleapis";
 import axios from "axios";

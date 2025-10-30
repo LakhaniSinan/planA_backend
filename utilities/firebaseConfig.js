8bb494c0bd2331b5d7a1d6005220727205d5efdb
LakhaniSinan <sinan.lakhani09@gmail.com>
2025-10-30 22:54:07 +0500

    Fix firebase config & notification system

--- utilities/firebaseConfig.js
+++ utilities/firebaseConfig.js
@@ -4,25 +4,18 @@ import admin from "firebase-admin";
 
 let serviceAccount;
 
-// If running on HEROKU (using BASE64 config)
 if (process.env.FIREBASE_CONFIG) {
-  serviceAccount = JSON.parse(
-    Buffer.from(process.env.FIREBASE_CONFIG, "base64").toString("utf8")
-  );
-}
-// If running locally (using JSON file)
-else {
-  serviceAccount = await import("../firebase-service-account.json", {
-    assert: { type: "json" },
-  }).then((m) => m.default);
+  const decoded = Buffer.from(process.env.FIREBASE_CONFIG, "base64").toString("utf8");
+  serviceAccount = JSON.parse(decoded);
+} else {
+  throw new Error("FIREBASE_CONFIG env var missing");
 }
 
-// Initialize Firebase only once
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
 
-export default admin;
 export { serviceAccount };
+export default admin;
